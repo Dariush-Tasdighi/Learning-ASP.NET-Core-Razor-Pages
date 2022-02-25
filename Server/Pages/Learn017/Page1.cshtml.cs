cb31ed4db39c40fb4594cdfbfbdeb446e5c1837b
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2022-02-25 19:26:21 +0330

    Making some changes!

---
+++ Server/Pages/Learn017/Page1.cshtml.cs
@@ -0,0 +1,14 @@
+namespace Server.Pages.Learn017
+{
+	public class Page1Model :
+		Microsoft.AspNetCore.Mvc.RazorPages.PageModel
+	{
+		public Page1Model() : base()
+		{
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
---
+++ Server/Pages/Learn017/Page2.cshtml.cs
@@ -0,0 +1,14 @@
+namespace Server.Pages.Learn017
+{
+	public class Page2Model :
+		Microsoft.AspNetCore.Mvc.RazorPages.PageModel
+	{
+		public Page2Model() : base()
+		{
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
