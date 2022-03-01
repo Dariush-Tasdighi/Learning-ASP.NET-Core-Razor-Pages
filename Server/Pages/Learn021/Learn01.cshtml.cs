a4217fca7919677c649bef27b25894cc94b49075
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2022-03-01 16:59:30 +0330

    Making some changes!

---
+++ Server/Pages/Learn021/Learn01.cshtml.cs
@@ -0,0 +1,14 @@
+namespace Server.Pages.Learn021
+{
+	public class Learn01Model :
+		Microsoft.AspNetCore.Mvc.RazorPages.PageModel
+	{
+		public Learn01Model() : base()
+		{
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
